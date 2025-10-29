59a83c04ff7d2cb5426e3d98392d6c87eb9c622f
Gakenye8741 <brayogakenye04@gmail.com>
2025-10-29 10:52:43 +0300

    Added Blocking and unblocking Logic

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -11,6 +11,7 @@ import { checkUserActive } from './Middlewares/checkUserActivity';
 import postRouter from './Services/posts/post.route';
 import messageRouter from './Services/Messages/message.route';
 import { rateLimiterMiddleware } from './Middlewares/rateLimiter';
+import blockRouters from './Services/Block/block.routes';
 
 dotenv.config();
 console.log("🟢 Scheduler file loaded");
@@ -42,6 +43,7 @@ app.use('/api', messageRouter);
 // ---------------------------- Protected Routes ----------------------------
 app.use('/api', anyAuth, checkUserActive, userRouters);
 app.use('/api', anyAuth, checkUserActive, postRouter);
+app.use('/api', anyAuth, checkUserActive, blockRouters);
 
 // 404 handler
 app.use((req, res) => {
